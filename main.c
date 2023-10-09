dde86956bdf9f5e4e9d2767e6fa95de3415f8a1b
Kendall Schmit <kendall504@me.com>
2023-10-09 12:31:10 -0700

    Improve reg.h, use 48MHz for system clock

--- main.c
+++ main.c
@@ -24,17 +24,26 @@
 
 int main(void)
 {
-    REG_RCC_AHBENR |= 1 << 19;
+    REG_RCC_CR2 |= BIT(16); // Set HSI48ON
+    // Wait for HSI48RDY
+    while (1) {
+        if (REG_RCC_CR2 & BIT(17)) {
+            break;
+        }
+    }
+    REG_RCC_CFGR |= BIT(1) | BIT(0); // Use HSI48 as system clock
 
-    REG_GPIOC_MODER |= 1 << ((7 * 2) + 0);
+
+    REG_RCC_AHBENR |= BIT(19);
+    REG_GPIOC_MODER |= BIT((7 * 2) + 0);
 
     while (1) {
-        REG_GPIOC_BSRR |= 1 << (7 + 16);
+        REG_GPIOC_BSRR |= BIT(7 + 16);
         for (int volatile i = 0; i < 100000; i++)
         {
             // Wait
         }
-        REG_GPIOC_BSRR |= 1 << (7);
+        REG_GPIOC_BSRR |= BIT(7);
         for (int volatile i = 0; i < 100000; i++)
         {
             // Wait
